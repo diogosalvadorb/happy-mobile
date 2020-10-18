00aa540f644ee789a2c2bfeb9d0fde8183745347
diogo <diogosbarbosa93@gmail.com>
2020-10-18 17:27:34 -0300

    form, position,  details

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     },
 
     title: {
-        fontFamily: 'Nunito_600SemiBod',
+        fontFamily: 'Nunito_600SemiBold',
         color: '#8fa7b3',
         fontSize: 16,
     },
